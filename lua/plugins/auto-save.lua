1be41524037b353f30c2c2706ad4906c92ed46df
kraxli <kraxli@pm.me>
2024-05-14 11:51:46 +0200

    autosave: change file name & no message/pop-up at each save

--- lua/plugins/auto-save.lua
+++
@@ -1,5 +0,0 @@
-return {
-  -- see: community.lua,     astrocommunity/editing-support /auto-save-nvim/
-  "auto-save-nvim",
-  event = { "User AstroFile", "InsertEnter", "FocusLost" }, --["InsertLeave", "TextChanged", "FocusLost"]
-}
