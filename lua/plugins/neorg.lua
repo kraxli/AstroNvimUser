74f1f469b4a8a3ce733473e4f2d3b28e0add53cc
kraxli <kraxli@pm.me>
2024-05-26 20:14:40 +0200

    luarocks install priority & disable neorg & subsystem

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,8 +1,18 @@
+-- Sources:
+-- https://vhyrro.github.io/posts/neorg-and-luarocks/
+-- https://github.com/nvim-neorg/neorg/issues/1342
+
 return {
   {
     "vhyrro/luarocks.nvim",
     enabled = false,
-    priority = 1000,
+    -- dependencies = {
+    --   "MunifTanjim/nui.nvim",
+    --   "nvim-neotest/nvim-nio",
+    --   "nvim-neorg/lua-utils.nvim",
+    --   "nvim-lua/plenary.nvim",
+    -- },
+    priority = 10000,
     config = true,
     -- build = "nvim -u NORC -l build.lua",
     -- potentially I need to trigger build process by `:Lazy build luarocks.nvim`
@@ -10,10 +20,16 @@ return {
   {
     "nvim-neorg/neorg",
     enabled = false,
-    dependencies = { "luarocks.nvim" },
+    dependencies = {
+      -- "vhyrro/luarocks.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-neotest/nvim-nio",
+      "nvim-neorg/lua-utils.nvim",
+      "nvim-lua/plenary.nvim",
+    },
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     -- version = "*", -- Pin Neorg to the latest stable release
-    version = "7.0.0", -- Pin Neorg to the latest stable release
+    version = "8.0.0", -- Pin Neorg to the latest stable release
     config = true,
     -- potentially run: Lazy build neorg
   },
