0265dc2f71461ce733182e90d20e183f9d2fb075
kraxli <kraxli@pm.me>
2024-05-27 22:58:31 +0200

    leap.nvim use m-key instead of default s-key (resolve conflict with mini.surround)

--- lua/plugins/leap-nvim.lua
+++ lua/plugins/leap-nvim.lua
@@ -16,16 +16,23 @@ return {
     "ggandor/leap.nvim",
     enabled = true,
     keys = {
-      { "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
-      { "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
-      { "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
+      -- s conflicts with "echasnovski/mini.surround"
+      -- { "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
+      -- { "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
+      -- { "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
+      --
+      -- use m instead
+      { "m", '<Plug>(leap-forward)', mode = { "n", "x", "o" }, desc = "Leap Forward to" },
+      { "M", '<Plug>(leap-backward)', mode = { "n", "x", "o" }, desc = "Leap Backward to" },
+      { "gm", '<Plug>(leap-from-window)', mode = { "n", "x", "o" }, desc = "Leap from Windows" },
+
     },
     config = function(_, opts)
       local leap = require "leap"
       for k, v in pairs(opts) do
         leap.opts[k] = v
       end
-      leap.add_default_mappings(true)
+      leap.add_default_mappings(false)
       vim.keymap.del({ "x", "o" }, "x")
       vim.keymap.del({ "x", "o" }, "X")
     end,
