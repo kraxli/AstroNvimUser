83afeeb92002f6f8f14f2e90b9a220b345cea854
kraxli <kraxli@pm.me>
2024-05-30 09:06:45 +0200

    enable treesitter for markdown on windows (after fix)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,11 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		enabled = function()
-			return not (vim.bo.filetype == "markdown" and vim.fn.has("win64") == 1)  -- jit.os:find("Windows")
-		end,
-	},
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	enabled = function()
+	-- 		return not (vim.bo.filetype == "markdown" and vim.fn.has("win64") == 1)  -- jit.os:find("Windows")
+	-- 	end,
+	-- 	opts = function (_, opts)
+	-- 			opts.auto_install = vim.fn.has("win64") == 0
+	-- 	end
+	-- },
 }
