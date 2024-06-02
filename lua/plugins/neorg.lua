558507c8c4ba3ccd8011e33edc75ea194c216ad3
kraxli <kraxli@pm.me>
2024-06-02 12:09:20 +0200

    neorg

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -6,31 +6,65 @@ return {
   {
     "vhyrro/luarocks.nvim",
     enabled = false,
-    -- dependencies = {
-    --   "MunifTanjim/nui.nvim",
-    --   "nvim-neotest/nvim-nio",
-    --   "nvim-neorg/lua-utils.nvim",
-    --   "nvim-lua/plenary.nvim",
-    -- },
-    priority = 10000,
+    priority = 1000,
     config = true,
-    -- build = "nvim -u NORC -l build.lua",
-    -- potentially I need to trigger build process by `:Lazy build luarocks.nvim`
   },
   {
     "nvim-neorg/neorg",
     enabled = false,
-    dependencies = {
-      -- "vhyrro/luarocks.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-neotest/nvim-nio",
-      "nvim-neorg/lua-utils.nvim",
-      "nvim-lua/plenary.nvim",
-    },
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    -- version = "*", -- Pin Neorg to the latest stable release
-    version = "8.0.0", -- Pin Neorg to the latest stable release
-    config = true,
-    -- potentially run: Lazy build neorg
+    dependencies = { "luarocks.nvim" },
+    version = "*",
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
   },
+
+  -- {
+  --   "vhyrro/luarocks.nvim",
+  --   -- enabled = false,
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-neotest/nvim-nio",
+  --     "nvim-neorg/lua-utils.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   priority = 10000,
+  --   config = true,
+  --   -- build = "nvim -u NORC -l build.lua",
+  --   -- potentially I need to trigger build process by `:Lazy build luarocks.nvim`
+  -- },
+  -- {
+  --   "nvim-neorg/neorg",
+  --   -- enabled = false,
+  --   dependencies = {
+  --     "vhyrro/luarocks.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-neotest/nvim-nio",
+  --     "nvim-neorg/lua-utils.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+  --   -- version = "*", -- Pin Neorg to the latest stable release
+  --   version = "8.0.0", -- Pin Neorg to the latest stable release
+  --   -- config = true,
+  --   config = function(_, opts) require("neorg").setup(opts) end,
+  --
+  --   -- potentially run: Lazy build neorg
+  -- },
 }
