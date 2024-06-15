e5d1c4d9906401cb77c14ee4de785e2006514416
kraxli <kraxli@pm.me>
2024-06-15 22:55:53 +0200

    markdown header increase / decrease

--- lua/raw.lua
+++ lua/raw.lua
@@ -18,3 +18,70 @@ vim.cmd [[
   vmap <c-c> y
   xmap <c-c> y
 ]]
+
+
+vim.cmd([[
+
+function! HeaderIncrease(...)
+    if a:0 == 0 | let lineNum = line('.') | else | let lineNum = a:1 | endif
+
+    let header_depth_max = 6
+
+    let current_line = getline(lineNum)
+    let num_hashs = len(substitute(current_line, '^\(\s*#*\).*$', '\1', ''))
+    let hashs2add = repeat('#', min([v:count1 + num_hashs, header_depth_max]))
+
+    if match(current_line, '^\s*#\{1,' .. header_depth_max .. '}\s') >= 0
+      " let subsi_str = '^\(\s*\)#\{1,' .. (header_depth_max-1) .. '} \(.*$\)'
+      let subsi_str = '^\(\s*\)#\{1,' .. (header_depth_max-1) .. '} \(.*$\)'
+      call setline(lineNum, substitute(current_line, subsi_str, '\1' .. hashs2add .. ' \2', ''))
+      return
+    endif
+    if match(current_line, '^\s*[^#]') >= 0
+      call setline(lineNum, substitute(current_line, '^\(\s*\)\(.*$\)', '\1' .. hashs2add .. ' \2', ''))
+      return
+    endif
+
+  endfunction
+
+  function! HeaderDecrease(...)
+    if a:0 == 0 | let lineNum = line('.') | else | let lineNum = a:1 | endif
+
+    let header_depth_max = 6
+
+    let current_line = getline(lineNum)
+    let hashs2subtract = v:count > 0 ? v:count : 1 
+    let num_hashs = len(substitute(current_line, '^\(\s*#*\).*$', '\1', ''))
+  
+    let num_hashs_new = num_hashs >= hashs2subtract ? num_hashs - hashs2subtract : header_depth_max 
+
+    if match(current_line, '^\s*#\{2,' .. header_depth_max .. '}\s') >= 0
+      " call setline(lineNum, substitute(current_line, '^\(\s*#\{1,' .. header_depth_max .. '}\)' .. repeat('#', num_hashs_new) .. ' \(.*$\)', '\1 \2', ''))
+      let subsi_str = '^\(\s*#\{1,' .. header_depth_max .. '}\) \(.*$\)'
+      call setline(lineNum, substitute(current_line, subsi_str, repeat('#', num_hashs_new) .. ' \2', ''))
+      return
+    endif
+    if match(current_line, '^\s*#\{1}\s') >= 0
+      call setline(lineNum, substitute(current_line, '^\(\s*\)# \(.*$\)', '\1\2', ''))
+      return
+    endif
+
+  endfunction
+
+    " see: https://github.com/kraxli/vim-mkd-org/blob/master/autoload/mkdd.vim
+    function! NumberedList(...) range
+      " or simply use !cat -n on selected range
+      if a:0 != 0 | let style=a:1 | else | let style='.' | endif
+      let line_start = getpos("'<")[1]
+      let line_end = getpos("'>")[1]
+
+      for num in range(line_start, line_end)
+        let current_line = getline(num)
+        let listNum = num - line_start + 1
+
+        " TODO allow for other list types as (capital) roman numbers, (capital) letters
+        call setline(num, substitute(current_line, '^\(\s*\)', '\1' . listNum . style . ' ', ''))
+      endfor
+    endfunction
+
+  ]])
