b1d490b5c2558699f2440c343bcb3588859cfaec
kraxli <kraxli@pm.me>
2024-11-28 21:20:28 +0100

    autosave: align config with plugin update

--- lua/plugins/auto-save-nvim.lua
+++ lua/plugins/auto-save-nvim.lua
@@ -5,19 +5,19 @@ return {
     event = { "InsertLeave", "InsertEnter", "FocusLost" }, --["InsertLeave", "TextChanged", "FocusLost"]  "User AstroFile",
     opts = {
       enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-      execution_message = {
-        enabled = false,
-        message = function() -- message to print on save
-          -- return ("AutoSave: saved at " .. vim.fn.strftime "%H:%M:%S")
-          return ""
-        end,
-        dim = 0.18, -- dim the color of `message`
-        cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-      },
+      -- execution_message = {
+      --   enabled = false,
+      --   message = function() -- message to print on save
+      --     -- return ("AutoSave: saved at " .. vim.fn.strftime "%H:%M:%S")
+      --     return ""
+      --   end,
+      --   dim = 0.18, -- dim the color of `message`
+      --   cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+      -- },
       trigger_events = { -- See :h events
         immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
         defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
-        cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+        cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
       },
       -- function that takes the buffer handle and determines whether to save the current buffer or not
       -- return true: if buffer is ok to be saved
