628323a28bfcf1322d0090a6d2baef3f8ce27bd8
kraxli <kraxli@pm.me>
2024-12-02 20:47:48 +0100

    comment bufdelete autocmds

--- lua/plugins/bufdelete-nvim.lua
+++ lua/plugins/bufdelete-nvim.lua
@@ -11,21 +11,21 @@ return {
         dirs = {},
         filetypes = {},
       },
-      autocmds = {
-        auto_bufdelete = {
-          {
-            event = "FileType",
-            desc = "Close",
-            pattern = { "*" },
-            callback = function()
-              if vim.bo.buftype ~= "terminal" or vim.bo.buftype ~= "term" then
-                vim.keymap.set( "n", "q", "<cmd>w!|Bdelete!<CR>", {  noremap = true, buffer = true, desc = "Close terminal" })
-                -- vim.keymap.set("n", "q", "<cmd>lua require('astrocore.buffer').close(0)<CR>", { noremap = true, buffer = true, desc = "Close terminal" })
-              end
-            end,
-          },
-        },
-      },
+      -- autocmds = {
+      --   auto_bufdelete = {
+      --     {
+      --       event = "FileType",
+      --       desc = "Close",
+      --       pattern = { "*" },
+      --       callback = function()
+      --         if vim.bo.buftype ~= "terminal" or vim.bo.buftype ~= "term" then
+      --           vim.keymap.set( "n", "q", "<cmd>w!|Bdelete!<CR>", {  noremap = true, buffer = true, desc = "Close terminal" })
+      --           -- vim.keymap.set("n", "q", "<cmd>lua require('astrocore.buffer').close(0)<CR>", { noremap = true, buffer = true, desc = "Close terminal" })
+      --         end
+      --       end,
+      --     },
+      --   },
+      -- },
     },
   },
 }
