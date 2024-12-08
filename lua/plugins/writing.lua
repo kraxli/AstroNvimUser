a84751d3d2b92b2a10f636d296693365532d6f21
kraxli <kraxli@pm.me>
2024-12-08 21:50:16 +0100

    vim-abolish: new words

--- lua/plugins/writing.lua
+++ lua/plugins/writing.lua
@@ -6,6 +6,8 @@ return {
       vim.cmd [[
   			Abolish {despa,sepe}rat{e,es,ed,ing,ely,ion,ions,or}  {despe,sepa}rat{}
   			Abolish {infal}{a,}{tion,ted} {infl}{a}{}
+  			Abolish {ulita,ulit}mate {ulti}mate 
+  			Abolish {anly,anyla,anali}sis {analy}sis
 			]]
     end,
   },
