7629f779f53f8f33eb0d9f13f267a19e8fd1e893
kraxli <kraxli@pm.me>
2025-01-12 22:21:03 +0100

    mappings: treesitter for spelling (disable which-key)

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -6,7 +6,7 @@ return {
     require("which-key").setup {
       plugins = {
         spelling = {
-          enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+          enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
           suggestions = 20, -- how many suggestions should be shown in the list?
         },
         presets = {
