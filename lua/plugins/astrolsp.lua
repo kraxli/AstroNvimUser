46e1cbe8ada972e11a45c84661da6456fb46bc53
kraxli <kraxli@pm.me>
2025-03-31 21:57:58 +0200

    astrolsp

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,5 +1,5 @@
 local servers = {} -- only add local servers if their commands are available
-for server, cmd in pairs { julials = "julia" } do
+for server, cmd in pairs { julials = "julia", r_language_server = "R" } do
   if vim.fn.executable(cmd) == 1 then table.insert(servers, server) end
 end
 
@@ -8,6 +8,15 @@ return {
   "AstroNvim/astrolsp",
   ---@type AstroLSPOpts
   opts = {
+    formatting = {
+      -- format_on_save = false, -- enable or disable automatic formatting on save
+      format_on_save = {
+        enabled = false,
+        ignore_filetypes = {}, -- disable format on save for specified filetypes
+        allow_filetypes = {},  -- only allow formatting on save for these filetypes
+      },
+    },
+    features = { signature_help = true },
     autocmds = {
       no_insert_inlay_hints = {
         cond = vim.lsp.inlay_hint and "textDocument/inlayHint" or false,
