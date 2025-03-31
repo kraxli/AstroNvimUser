9084d4a05419b8461e7c5f4baf405965f3921419
kraxli <kraxli@pm.me>
2025-03-31 22:33:38 +0200

    improve blink.cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -9,19 +9,24 @@ return {
     },
     signature = { enabled = true },
     cmdline = {
-      completion = { menu = { auto_show = true } },
+      enabled = true,
+      completion = { menu = { auto_show = false } },
       keymap = {
         -- recommended, as the default keymap will only show and select the next item
         ["<Up>"] = { "select_prev", "fallback" },
         ["<Down>"] = { "select_next", "fallback" },
         ["<Tab>"] = { "show", "accept" },
-        -- ["<Tab>"] = {
-        --   function(cmp)
-        --     if cmp.is_ghost_text_visible() and not cmp.is_menu_visible() then return cmp.accept() end
-        --   end,
-        --   "show_and_insert",
-        --   "select_next",
-        -- },
+        ["<Right>"] = {
+          function(cmp)
+            if cmp.is_ghost_text_visible() and not cmp.is_menu_visible() then return cmp.accept() end
+          end,
+          "show_and_insert",
+          "accept", --"select_next",
+        },
+        -- ["<Right>"] = { "show", "accept" },
+        ['<CR>'] = {'accept_and_enter'},
+        ['<Left>'] = { 'cancel' },
+        ['<ESC>'] = { 'cancel' },
       },
     },
   },
