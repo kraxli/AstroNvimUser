511c9f76d7f5060938acc838daa6a6d055290f5b
kraxli <kraxli@pm.me>
2025-04-05 21:56:46 +0200

    customize lazy_setup

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -1,17 +1,17 @@
 require("lazy").setup({
-  {
-    "AstroNvim/AstroNvim",
-    version = "^5", -- Remove version tracking to elect for nighly AstroNvim
-    import = "astronvim.plugins",
-    opts = { -- AstroNvim options must be set here with the `import` key
-      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
-      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
-      -- icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
-      -- pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
-      -- update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
-    },
-  },
-  { import = "community" },
+  -- {
+  --   "AstroNvim/AstroNvim",
+  --   version = "^5", -- Remove version tracking to elect for nighly AstroNvim
+  --   import = "astronvim.plugins",
+  --   opts = { -- AstroNvim options must be set here with the `import` key
+  --     mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
+  --     maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
+  --     -- icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
+  --     -- pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
+  --     -- update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
+  --   },
+  -- },
+  -- { import = "community" },
   { import = "base" },
   { import = "plugins" },
   -- { import = "commands" },
@@ -29,10 +29,10 @@ require("lazy").setup({
   defaults = { lazy = true },
   diff = { cmd = "terminal_git" },
 
-  checker = vim.fn.has('unix') == 1 and {enabled = true } or {enabled = false, concurrency = 1 },
-  wait = vim.fn.has('unix') == 1 and false or true,
-  -- concurrency = jit.os:find("Windows") and (vim.uv.available_parallelism() * 2) or nil,
-  concurrency = jit.os:find("Windows") and 1 or nil,
+  -- checker = vim.fn.has('unix') == 1 and {enabled = true } or {enabled = false, concurrency = 1 },
+  -- wait = vim.fn.has('unix') == 1 and false or true,
+  -- -- concurrency = jit.os:find("Windows") and (vim.uv.available_parallelism() * 2) or nil,
+  -- concurrency = jit.os:find("Windows") and 1 or nil,
 
   install = { colorscheme = { "catppuccin", "astrodark", "habamax" } },
   lockfile = vim.fn.stdpath "data" .. "/lazy-lock.json",
