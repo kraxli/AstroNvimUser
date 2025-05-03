231151b9a6edb5ed57adbeded75d871c410267a7
kraxli <kraxli@pm.me>
2025-05-03 20:37:47 +0200

    disable colorscheme caching

--- lua/plugins/cache-colorscheme.lua
+++ lua/plugins/cache-colorscheme.lua
@@ -1,33 +1,34 @@
--- pick a location to cache colorscheme
-local colorscheme_cache = vim.fs.joinpath(vim.fn.stdpath "state" --[[@as string]], "last_colorscheme")
-
----@type LazySpec
-return {
-  {
-    "AstroNvim/astroui",
-    --@param opts AstroUIOpts
-    opts = function(_, opts)
-      -- read colorscheme cache on open
-      require("astrocore").with_file(colorscheme_cache, "r", function(file) opts.colorscheme = file:read "*a" end)
-    end,
-  },
-  {
-    "AstroNvim/astrocore",
-    ---@type AstroCoreOpts
-    opts = {
-      autocmds = {
-        -- save colorscheme to cache on change
-        cache_colorscheme = {
-          {
-            event = "ColorScheme",
-            callback = function(args)
-              if args.match then
-                require("astrocore").with_file(colorscheme_cache, "w+", function(file) file:write(args.match) end)
-              end
-            end,
-          },
-        },
-      },
-    },
-  },
-}
+-- -- pick a location to cache colorscheme
+-- local colorscheme_cache = vim.fs.joinpath(vim.fn.stdpath "state" --[[@as string]], "last_colorscheme")
+--
+-- ---@type LazySpec
+-- return {
+--   {
+--     "AstroNvim/astroui",
+--     --@param opts AstroUIOpts
+--     opts = function(_, opts)
+--       -- read colorscheme cache on open
+--       require("astrocore").with_file(colorscheme_cache, "r", function(file) opts.colorscheme = file:read "*a" end)
+--     end,
+--   },
+--   {
+--     "AstroNvim/astrocore",
+--     ---@type AstroCoreOpts
+--     opts = {
+--       autocmds = {
+--         -- save colorscheme to cache on change
+--         cache_colorscheme = {
+--           {
+--             event = "ColorScheme",
+--             callback = function(args)
+--               if args.match then
+--                 require("astrocore").with_file(colorscheme_cache, "w+", function(file) file:write(args.match) end)
+--               end
+--             end,
+--           },
+--         },
+--       },
+--     },
+--   },
+-- }
+return {}
