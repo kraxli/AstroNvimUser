c628b7b802287096c8bce7ff8e0620df00e0b3d2
kraxli <kraxli@pm.me>
2025-09-11 15:30:11 +0200

    render-markdown update (not used)

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,24 +1,24 @@
 ---@type LazySpec
--- return {
---   "MeanderingProgrammer/render-markdown.nvim",
---   enabled = false,
---   cmd = "RenderMarkdown",
---   ft = function()
---     local plugin = require("lazy.core.config").spec.plugins["render-markdown.nvim"]
---     local opts = require("lazy.core.plugin").values(plugin, "opts", false)
---     return opts.file_types or { "markdown" }
---   end,
---   opts = {
---     completions = { lsp = { enabled = true } },
---     pipe_table = { enabled = false },
---     sign = { enabled = false },
---   },
---   specs = {
---     {
---       "catppuccin",
---       optional = true,
---       ---@type CatppuccinOptions
---       opts = { integrations = { render_markdown = true } },
---     },
---   },
--- }
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  enabled = false,
+  cmd = "RenderMarkdown",
+  ft = function()
+    local plugin = require("lazy.core.config").spec.plugins["render-markdown.nvim"]
+    local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+    return table.insert(opts.file_types, "quarto") or { "markdown", "quarto" }
+  end,
+  opts = {
+    completions = { lsp = { enabled = true } },
+    pipe_table = { enabled = false },
+    sign = { enabled = false },
+  },
+  specs = {
+    {
+      "catppuccin",
+      optional = true,
+      ---@type CatppuccinOptions
+      opts = { integrations = { render_markdown = true } },
+    },
+  },
+}
