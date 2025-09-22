cba7c632ac314de534a44598b4316ddd50944321
kraxli <kraxli@pm.me>
2025-09-22 09:49:12 +0200

    formatting: replace tabs with spaces

--- lua/plugins/writing.lua
+++ lua/plugins/writing.lua
@@ -85,7 +85,7 @@ return {
   },
   {
     "toppair/peek.nvim",
-    enabled = vim.fn.has('win64') == 1,
+    -- enabled = vim.fn.has('win64') == 1,
     lazy = true,
     build = "deno task --quiet build:fast",
     dependencies = {
@@ -165,58 +165,58 @@ return {
     -- init
     config = function()
       vim.cmd [[
-			  let g:bullets_delete_last_bullet_if_empty = 1
-			  let g:bullets_auto_indent_after_colon = 1
-			  let g:bullets_max_alpha_characters = 2
-			  let g:bullets_renumber_on_change = 1
-			  let g:bullets_nested_checkboxes = 1
-			  let g:bullets_checkbox_markers = ' .oOx'  " '✗○◐●✓'
-			  let g:bullets_checkbox_partials_toggle = 1
-			  let g:bullets_set_mappings = 0 " disable adding default key mappings, default = 1
-			  let g:bullets_outline_levels = ['ROM', 'ABC', 'num', 'abc', 'rom', 'std-' ]   " -- 'std*', 'std+'
+        let g:bullets_delete_last_bullet_if_empty = 1
+        let g:bullets_auto_indent_after_colon = 1
+        let g:bullets_max_alpha_characters = 2
+        let g:bullets_renumber_on_change = 1
+        let g:bullets_nested_checkboxes = 1
+        let g:bullets_checkbox_markers = ' .oOx'  " '✗○◐●✓'
+        let g:bullets_checkbox_partials_toggle = 1
+        let g:bullets_set_mappings = 0 " disable adding default key mappings, default = 1
+        let g:bullets_outline_levels = ['ROM', 'ABC', 'num', 'abc', 'rom', 'std-' ]   " -- 'std*', 'std+'
 
-				" default = []
-				" N.B. You can set these mappings as-is without using this g:bullets_custom_mappings option but it
-				" will apply in this case for all file types while when using g:bullets_custom_mappings it would
-				" take into account file types filter set in g:bullets_enabled_file_types, and also
-				" g:bullets_enable_in_empty_buffers option.
-				let g:bullets_custom_mappings = [
-  				\ ['imap', '<cr>', '<Plug>(bullets-newline)'],
-  				\ ['inoremap', '<C-cr>', '<cr>'],
-  				\
-  				\ ['noremap', 'O', 'k<Plug>(bullets-newline)'],
-  				\ ['nmap', 'o', '<Plug>(bullets-newline)'],
-  				\
-  				\ ['vmap', 'gN', '<Plug>(bullets-renumber)'],
-  				\ ['nmap', 'gN', '<Plug>(bullets-renumber)'],
-  				\
-  				\ ['imap', '<C-t>', '<Plug>(bullets-demote)'],
-  				\ ['nmap', '>>', '<Plug>(bullets-demote)'],
-  				\ ['nmap', '> ', '<Plug>(bullets-demote)'],
-  				\ ['vmap', '>', '<Plug>(bullets-demote)'],
-  				\ ['imap', '<C-d>', '<Plug>(bullets-promote)'],
-  				\ ['nmap', '<<', '<Plug>(bullets-promote)'],
-  				\ ['vmap', '<', '<Plug>(bullets-promote)'],
-  				\ ['vmap', '< ', '<Plug>(bullets-promote)'],
-  				\ ]
-  				" \ ['nmap', '<c-space>', '<Plug>(bullets-toggle-checkbox)'],
-  				" \ ['nmap', 'zt', '<Plug>(bullets-toggle-checkbox)'],
-  				" \
+        " default = []
+        " N.B. You can set these mappings as-is without using this g:bullets_custom_mappings option but it
+        " will apply in this case for all file types while when using g:bullets_custom_mappings it would
+        " take into account file types filter set in g:bullets_enabled_file_types, and also
+        " g:bullets_enable_in_empty_buffers option.
+        let g:bullets_custom_mappings = [
+          \ ['imap', '<cr>', '<Plug>(bullets-newline)'],
+          \ ['inoremap', '<C-cr>', '<cr>'],
+          \
+          \ ['noremap', 'O', 'k<Plug>(bullets-newline)'],
+          \ ['nmap', 'o', '<Plug>(bullets-newline)'],
+          \
+          \ ['vmap', 'gN', '<Plug>(bullets-renumber)'],
+          \ ['nmap', 'gN', '<Plug>(bullets-renumber)'],
+          \
+          \ ['imap', '<C-t>', '<Plug>(bullets-demote)'],
+          \ ['nmap', '>>', '<Plug>(bullets-demote)'],
+          \ ['nmap', '> ', '<Plug>(bullets-demote)'],
+          \ ['vmap', '>', '<Plug>(bullets-demote)'],
+          \ ['imap', '<C-d>', '<Plug>(bullets-promote)'],
+          \ ['nmap', '<<', '<Plug>(bullets-promote)'],
+          \ ['vmap', '<', '<Plug>(bullets-promote)'],
+          \ ['vmap', '< ', '<Plug>(bullets-promote)'],
+          \ ]
+          " \ ['nmap', '<c-space>', '<Plug>(bullets-toggle-checkbox)'],
+          " \ ['nmap', 'zt', '<Plug>(bullets-toggle-checkbox)'],
+          " \
 
-  			imap      <C-CR>    <Plug>(bullets-newline)
-  			" inoremap  <C-cr>  <cr>
-			  noremap   O       k<Plug>(bullets-newline)
-  			nmap      o       <Plug>(bullets-newline)
-  			vmap      gN      <Plug>(bullets-renumber)
-  			nmap      gN      <Plug>(bullets-renumber)
-	      imap      <C-t>   <Plug>(bullets-demote)
-  			nmap      gD      <Plug>(bullets-demote)
-  			nmap      '> '    <Plug>(bullets-demote)
-  			vmap      '> '    <Plug>(bullets-demote)
-  			imap      <C-d>   <Plug>(bullets-promote)
-  			nmap      gP      <Plug>(bullets-promote)
-  			vmap      '< '    <Plug>(bullets-promote)
-				]]
+        imap      <C-CR>    <Plug>(bullets-newline)
+        " inoremap  <C-cr>  <cr>
+        noremap   O       k<Plug>(bullets-newline)
+        nmap      o       <Plug>(bullets-newline)
+        vmap      gN      <Plug>(bullets-renumber)
+        nmap      gN      <Plug>(bullets-renumber)
+        imap      <C-t>   <Plug>(bullets-demote)
+        nmap      gD      <Plug>(bullets-demote)
+        nmap      '> '    <Plug>(bullets-demote)
+        vmap      '> '    <Plug>(bullets-demote)
+        imap      <C-d>   <Plug>(bullets-promote)
+        nmap      gP      <Plug>(bullets-promote)
+        vmap      '< '    <Plug>(bullets-promote)
+        ]]
 
       vim.keymap.set("n", "<leader>zt", "<cmd>lua require('utils').handle_checkbox_bullets()<CR>")
       -- vim.keymap.set("n", "<C-space>", "<cmd>lua require('utils').handle_checkbox_bullets()<CR>")
