b3bc1bf9f30ca13b95b0a54fe1931beb003dade0
Micah Halter <micah.halter@gtri.gatech.edu>
2025-10-20 11:45:19 -0400

    feat(snacks): add mapping to hover preview an image

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -26,4 +26,28 @@ return {
       },
     },
   },
+  specs = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>i"] = {
+              function()
+                local image = require "snacks.image"
+                image.doc.hover_close()
+                image.hover()
+                vim.api.nvim_create_autocmd("CursorMoved", {
+                  once = true,
+                  group = vim.api.nvim_create_augroup("snacks_image_cleanup", { clear = true }),
+                  callback = function() image.doc.hover_close() end,
+                })
+              end,
+              desc = "Hover image",
+            },
+          },
+        },
+      },
+    },
+  },
 }
