7dad5af96df18fa56eb28fc9989b5f264ee7f45c
kraxli <kraxli@pm.me>
2025-11-10 16:31:58 +0100

    check if table opts.filetype exists

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -5,7 +5,12 @@ return {
   cmd = "RenderMarkdown",
   ft = function()
     local plugin = require("lazy.core.config").spec.plugins["render-markdown.nvim"]
+    
     local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+    if opts.file_types == nil then
+      opts.file_types = {}
+    end
+
     return table.insert(opts.file_types, "quarto") or { "markdown", "quarto" }
   end,
   opts = {
