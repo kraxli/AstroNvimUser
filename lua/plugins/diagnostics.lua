521b43a69ce27a2514a6dcd2d1115a5b8eee199d
kraxli <kraxli@pm.me>
2026-04-25 23:14:48 +0200

    move diagnostics config to own file

---
+++ lua/plugins/diagnostics.lua
@@ -0,0 +1,28 @@
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    features = {
+      -- toggle if diagnostics are enabled on startup
+      -- diagnostics = true,
+      diagnostics = { virtual_text = true, virtual_lines = false },
+    },
+    -- Configuration passed to `vim.diagnostic.config()`
+    -- All available options can be found with `:h vim.diagnostic.Opts`
+    diagnostics = {
+      virtual_text = {
+        current_line = true,
+        severity = { min = vim.diagnostic.severity.HINT },
+        -- severity = { min = vim.diagnostic.severity.WARN },
+      },
+      -- virtual_text = { current_line = true, severity = { max = vim.diagnostic.severity.INFO } },
+      -- virtual_text = true,
+      virtual_lines = false,
+      -- virtual_lines = { current_line = true, severity = { min = vim.diagnostic.severity.WARN } },
+      -- virtual_lines = { current_line = true, severity = { min = vim.diagnostic.severity.ERROR } },
+      update_in_insert = false,
+      underline = false,
+      severity_sort = true,
+    },
+  },
+}
