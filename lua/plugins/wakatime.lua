c31e9104269f79d58a44b1bfd327db8f90ceddab
Micah Halter <micah.halter@gtri.gatech.edu>
2026-04-28 11:12:32 -0400

    chore: disable wakatime if on vpn

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -1,2 +1,9 @@
 ---@type LazySpec
-return { "wakatime/vim-wakatime", lazy = false }
+return {
+  "wakatime/vim-wakatime",
+  lazy = false,
+  cond = function()
+    vim.fn.system "ip addr | grep -E 'tun|wg|ppp'"
+    return vim.v.shell_error ~= 0
+  end,
+}
