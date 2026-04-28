aaf11c52940fe3da297bed823e14da8c0d36449a
kraxli <kraxli@pm.me>
2026-04-28 22:19:45 +0200

    fix smart-split mappings after rebase to mh

--- lua/plugins/mappingsX.lua
+++ lua/plugins/mappingsX.lua
@@ -135,18 +135,6 @@ return {
             desc = "xxx",
             -- ["x"] = { "<cmd>lua require('telescope.builtin').buffers()<CR>", "Buffers list" },
           },
-
-          -- resize smart-splits
-          ["<C-Down>"] = false,
-          ["<C-Left>"] = false,
-          ["<C-Right>"] = false,
-          ["<C-Up>"] = false,
-
-          -- resize with arrows
-          ["<C-Up>"] = { function() require("smart-splits").resize_up(2) end, desc = "Resize split up" },
-          ["<C-Down>"] = { function() require("smart-splits").resize_down(2) end, desc = "Resize split down" },
-          ["<C-Left>"] = { function() require("smart-splits").resize_left(2) end, desc = "Resize split left" },
-          ["<C-Right>"] = { function() require("smart-splits").resize_right(2) end, desc = "Resize split right" },
         },
         t = {
           -- setting a mapping to false will disable it
