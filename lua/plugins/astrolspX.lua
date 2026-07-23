5d68a982713656ccb33f58d877cb5561b2a91190
kraxli <kraxli@pm.me>
2026-07-23 22:07:49 +0200

    lsp keymap for "search usage / reference"

--- lua/plugins/astrolspX.lua
+++ lua/plugins/astrolspX.lua
@@ -1,3 +1,5 @@
+
+local prefix_lsp = "<leader>l"
 local servers = {} -- only add local servers if their commands are available
 for server, cmd in pairs { julials = "julia", r_language_server = "R" } do
   if vim.fn.executable(cmd) == 1 then table.insert(servers, server) end
@@ -15,6 +17,12 @@ return {
         ignore_filetypes = {}, -- disable format on save for specified filetypes
         allow_filetypes = {}, -- only allow formatting on save for these filetypes
       },
+    },
+    mappings = {
+       n = {
+         -- [prefix] = { name = " LSP" },
+         [prefix_lsp .. "u"] = { function() vim.lsp.buf.references() end, desc = "Search usage / ref.", cond = "textDocument/references" },
+       }
     },
   },
 }
